23ccbcdd2b36dab56a34ff8b96da70888a23ec8d
K0M0D4 <comoto45@gmail.com>
2021-04-21 17:50:06 +0200

    changed colors system

--- src/colors.hpp
+++ src/colors.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <cstdlib>
 
 enum Color {
     BLACK,
@@ -13,68 +14,35 @@ enum Color {
     WHITE
 };
 
-#if defined(_WIN32)
-    #include <windows.h>
+void SetColor(Color color) {
+    std::string colVal;
 
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::system("setlocal");
 
-    void SetColor(Color color) {
-        switch(color) {
-        case BLACK:
-            SetConsoleTextAttribute(hOut, 0);
-        break;
-        case RED:
-            SetConsoleTextAttribute(hOut, FOREGROUND_RED);
-        break;
-        case GREEN:
-            SetConsoleTextAttribute(hOut, FOREGROUND_GREEN);
-        break;
-        case YELLOW:
-            SetConsoleTextAttribute(hOut, FOREGROUND_RED | FOREGROUND_GREEN);
-        break;
-        case BLUE:
-            SetConsoleTextAttribute(hOut, FOREGROUND_BLUE);
-        break;
-        case MAGENTA:
-            SetConsoleTextAttribute(hOut, FOREGROUND_RED | FOREGROUND_BLUE);
-        break;
-        case CYAN:
-            SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_BLUE);
-        break;
-        case WHITE:
-            SetConsoleTextAttribute(hOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-        }
+    switch(color) {
+    case BLACK:
+        colVal = "\x1B[30m";
+    break;
+    case RED:
+        colVal = "\x1B[31m";
+    break;
+    case GREEN:
+        colVal = "\x1B[32m";
+    break;
+    case YELLOW:
+        colVal = "\x1B[33m";
+    break;
+    case BLUE:
+        colVal = "\x1B[34m";
+    break;
+    case MAGENTA:
+        colVal = "\x1B[35m";
+    break;
+    case CYAN:
+        colVal = "\x1B[36m";
+    break;
+    case WHITE:
+        colVal = "\x1B[37m";
     }
-#elif defined(__linux__)
-    void SetColor(Color color) {
-        std::string colVal;
-
-        switch(color) {
-        case BLACK:
-            colVal = "\x1B[30m";
-        break;
-        case RED:
-            colVal = "\x1B[31m";
-        break;
-        case GREEN:
-            colVal = "\x1B[32m";
-        break;
-        case YELLOW:
-            colVal = "\x1B[33m";
-        break;
-        case BLUE:
-            colVal = "\x1B[34m";
-        break;
-        case MAGENTA:
-            colVal = "\x1B[35m";
-        break;
-        case CYAN:
-            colVal = "\x1B[36m";
-        break;
-        case WHITE:
-            colVal = "\x1B[37m";
-        }
-
-        std::cout << colVal;
-    }
-#endif
+    std::cout << colVal;
+}
